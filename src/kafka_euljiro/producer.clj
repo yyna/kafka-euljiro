7cf7a0f581bc8f22db9a23e935e40bf3e2be756d
wickedev <orange881217@gmail.com>
2023-03-14 14:36:29 +0900

    consumer code

--- src/kafka_euljiro/producer.clj
+++ src/kafka_euljiro/producer.clj
@@ -4,12 +4,17 @@
    [medley.core :refer [update-existing]]
    [clojure.walk :as walk])
   (:import
-   (org.apache.kafka.clients.producer KafkaProducer ProducerRecord)))
+   (org.apache.kafka.clients.producer KafkaProducer ProducerRecord)
+   [org.apache.kafka.clients.consumer KafkaConsumer]))
 
 
 (defn produce!
   [producer topic message]
   (.send producer (ProducerRecord. topic (json/write-str message))))
+
+(defn consume!
+  [consumer topic]
+  (.subscribe consumer (java.util.ArrayList. [topic])))
 
 (comment
   ;; KafkProducer 만들기
@@ -21,11 +26,22 @@
                                      (update-existing :retries int)
                                      (walk/stringify-keys))))
 
+  @(def consumer (KafkaConsumer. (-> {:bootstrap.servers "localhost:29092"
+                                      :key.deserializer    "org.apache.kafka.common.serialization.StringDeserializer"
+                                      :value.deserializer  "org.apache.kafka.common.serialization.StringDeserializer"
+                                      :group.id "euljiro-kafka-jungin-1"}
+                                     (update-existing :retries int)
+                                     (walk/stringify-keys))))
+
   ;; topic 이름
-  @(def topic-name "kafka-euljiro-jungin")
+  @(def topic-name "euljiro-kafka-jungin")
 
 
   ;; Message produce하기
   (produce! producer topic-name {:hello :world})
+  (consume! consumer topic-name)
+
+  (for [record (.poll consumer 100)]
+    (prn (json/read-str (.value record))))
 
   :rcf)
